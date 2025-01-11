caaed2f9b024867284d24734cf5d0c86cd9794af
codemiguel-dev <jose.calderon15@inacapmail.cl>
2025-01-11 04:14:15 -0300

    update

--- main__.py
+++ main__.py
@@ -5,12 +5,38 @@ imagenes = [
     "img/img-1.png",
     "img/img-1.png",
     "img/img-1.png",
+    "img/img-1.png",
+    "img/img-1.png",
+    "img/img-2.png",
+    "img/img-2.png",
     "img/img-2.png",
     "img/img-2.png",
     "img/img-2.png",
     "img/img-3.png",
     "img/img-3.png",
     "img/img-3.png",
+    "img/img-3.png",
+    "img/img-3.png",
+    "img/img-4.png",
+    "img/img-4.png",
+    "img/img-4.png",
+    "img/img-4.png",
+    "img/img-4.png",
+    "img/img-5.png",
+    "img/img-5.png",
+    "img/img-5.png",
+    "img/img-5.png",
+    "img/img-5.png",
+    "img/img-6.png",
+    "img/img-6.png",
+    "img/img-6.png",
+    "img/img-6.png",
+    "img/img-6.png",
+    "img/img-7.png",
+    "img/img-7.png",
+    "img/img-7.png",
+    "img/img-7.png",
+    "img/img-7.png",
 ]
 
 # Abrir las imágenes
@@ -18,7 +44,7 @@ frames = [Image.open(imagen) for imagen in imagenes]
 
 # Crear el GIF
 frames[0].save(
-    "theme-login.gif",
+    "theme-register.gif",
     save_all=True,
     append_images=frames[1:],
     duration=200,  # Duración de cada cuadro en milisegundos
