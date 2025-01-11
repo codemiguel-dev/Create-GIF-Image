8ba1d8bcc489e902caa71bb10bba28088c5d0005
codemiguel-dev <jose.calderon15@inacapmail.cl>
2025-01-11 02:23:58 -0300

    update

--- main__.py
+++ main__.py
@@ -1,14 +1,24 @@
 from PIL import Image
 
 # Lista de imágenes (asegúrate de que sean del mismo tamaño)
-imagenes = ["img/img-1.png", "img/img-2.png"]
+imagenes = [
+    "img/img-1.png",
+    "img/img-1.png",
+    "img/img-1.png",
+    "img/img-2.png",
+    "img/img-2.png",
+    "img/img-2.png",
+    "img/img-3.png",
+    "img/img-3.png",
+    "img/img-3.png",
+]
 
 # Abrir las imágenes
 frames = [Image.open(imagen) for imagen in imagenes]
 
 # Crear el GIF
 frames[0].save(
-    "mi_animacion.gif",
+    "theme-login.gif",
     save_all=True,
     append_images=frames[1:],
     duration=200,  # Duración de cada cuadro en milisegundos
