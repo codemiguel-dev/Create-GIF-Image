9d7c95b191dcd5f3697d6f33423d61bce26fdaca
codemiguel-dev <jose.calderon15@inacapmail.cl>
2025-01-12 02:20:57 -0300

    update

--- main__.py
+++ main__.py
@@ -32,6 +32,31 @@ imagenes = [
     "img/img-6.png",
     "img/img-6.png",
     "img/img-6.png",
+    "img/img-7.png",
+    "img/img-7.png",
+    "img/img-7.png",
+    "img/img-7.png",
+    "img/img-7.png",
+    "img/img-8.png",
+    "img/img-8.png",
+    "img/img-8.png",
+    "img/img-8.png",
+    "img/img-8.png",
+    "img/img-9.png",
+    "img/img-9.png",
+    "img/img-9.png",
+    "img/img-9.png",
+    "img/img-9.png",
+    "img/img-10.png",
+    "img/img-10.png",
+    "img/img-10.png",
+    "img/img-10.png",
+    "img/img-10.png",
+    "img/img-11.png",
+    "img/img-11.png",
+    "img/img-11.png",
+    "img/img-11.png",
+    "img/img-11.png",
 ]
 
 # Abrir las imágenes
@@ -39,7 +64,7 @@ frames = [Image.open(imagen) for imagen in imagenes]
 
 # Crear el GIF
 frames[0].save(
-    "theme-register.gif",
+    "theme-register-user-admin.gif",
     save_all=True,
     append_images=frames[1:],
     duration=200,  # Duración de cada cuadro en milisegundos
