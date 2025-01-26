3ff674967908d9f1f3920cbffaa94e14807d3abd
codemiguel-dev <jose.calderon15@inacapmail.cl>
2025-01-26 02:36:06 -0300

    update

--- main__.py
+++ main__.py
@@ -4,9 +4,6 @@ from PIL import Image
 imagenes = [
     "img/img-1.png",
     "img/img-2.png",
-    "img/img-3.png",
-    "img/img-4.png",
-    "img/img-5.png",
 ]
 
 # Abrir las imágenes
@@ -14,7 +11,7 @@ frames = [Image.open(imagen) for imagen in imagenes]
 
 # Crear el GIF
 frames[0].save(
-    "theme-search-client-invoice-admin.gif",
+    "theme-client-admin.gif",
     save_all=True,
     append_images=frames[1:],
     duration=2000,  # Duración de cada cuadro en milisegundos
