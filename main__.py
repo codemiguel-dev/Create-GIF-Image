edf3e9ee32e378e30cb5693a9494fef2c533ae95
codemiguel-dev <jose.calderon15@inacapmail.cl>
2025-01-26 22:33:40 -0300

    update

--- main__.py
+++ main__.py
@@ -13,7 +13,6 @@ imagenes = [
     "img/img-9.png",
     "img/img-10.png",
     "img/img-11.png",
-    "img/img-12.png",
 ]
 
 # Abrir las imágenes
@@ -21,7 +20,7 @@ frames = [Image.open(imagen) for imagen in imagenes]
 
 # Crear el GIF
 frames[0].save(
-    "theme-add-client-admin.gif",
+    "theme-update-client-admin.gif",
     save_all=True,
     append_images=frames[1:],
     duration=2000,  # Duración de cada cuadro en milisegundos
